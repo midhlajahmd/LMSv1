1cb8725de4052ccfe6a7c87790ced5edc7261cda
midhlaj <midhlajahmdtk@gmail.com>
2024-12-04 17:08:49 +0530

    created models

--- LMSv1/librarymsv1/models.py
+++ LMSv1/librarymsv1/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+#model for GENRES
+class Genres(models.Model):
+    genre_name= models.CharField(max_length=150)
+
+    #order by
+    class Meta:
+        ordering= ['genre_name']
+
+    def __str__(self):
+        return self.genre
+    
+#model for AUTHORS
+class Authors(models.Model):
+    author_name= models.CharField(max_length=150)
+
+    #order by
+    class Meta:
+        ordering= ['author_name']
+    
+    def __str__(self):
+        return self.author
+    
+#model for BOOKS
+class Books(models.Model):
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('unavailable', 'Unavailable'),
+    ]
+    book_name= models.CharField(max_length=200)
+    genre= models.ForeignKey(Genres, related_name='book_genre',on_delete=models.SET_NULL, null=True)
+    author= models.ForeignKey(Authors, related_name='book_author',on_delete=models.SET_NULL, null=True)
+    ISBN = models.CharField(max_length=13, unique=True)  
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    rent = models.DecimalField(max_digits=8, decimal_places=2)
+    status= models.CharField(max_length=13, choices=STATUS_CHOICES, default='available')
+
+    def __str__(self):
+        return self.book_name
+
+#model for MEMEBRSHIP PLANS
+#class Membership(models.Model):
